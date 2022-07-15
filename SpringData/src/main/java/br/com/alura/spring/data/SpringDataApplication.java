7b8df61d88cde55b77941806788bdfde78e3d29f
Douglas Felipe da Silva <dev.douggo@gmail.com>
2022-07-15 10:43:20 -0300

    Finalizada aula 01 - primeiro repositório

--- SpringData/src/main/java/br/com/alura/spring/data/SpringDataApplication.java
+++ SpringData/src/main/java/br/com/alura/spring/data/SpringDataApplication.java
@@ -1,14 +1,30 @@
 package br.com.alura.spring.data;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-public class SpringDataApplication {
+import br.com.alura.spring.data.orm.Cargo;
+import br.com.alura.spring.data.repository.CargoRepository;
+
+@SpringBootApplication
+public class SpringDataApplication implements CommandLineRunner {
+
+	private final CargoRepository repository;
+
+	public SpringDataApplication(CargoRepository cargoRepository) {
+		this.repository = cargoRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Cargo cargo = new Cargo();
+		cargo.setDescricao("Arquiteto de Software");
+		this.repository.save(cargo);
 	}
 
 }
