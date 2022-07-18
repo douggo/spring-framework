22956160c269c2f360d82068ddb2e26cc797478c
Douglas Felipe da Silva <dev.douggo@gmail.com>
2022-07-18 15:59:53 -0300

    Finalizada aula 02 - publicando endpoints

--- SpringBootApiRest/src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ SpringBootApiRest/src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -3,21 +3,20 @@ package br.com.alura.forum.controller;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.model.Curso;
 import br.com.alura.forum.model.Topico;
 
-@Controller
+@RestController
 public class TopicoController {
     
     @RequestMapping("/topicos")
-    @ResponseBody
-    public List<Topico> lista() {
+    public List<TopicoDto> lista() {
         Topico topico = new Topico("Dúvida", "Dúvida sobre Spring", new Curso("Spring Boot API Rest", "Programação"));
-        return Arrays.asList(topico, topico, topico);
+        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
     }
 
 }
