0bd99adf7caa32d7062e5e4e3321bc8aea6bcf18
Douglas Felipe da Silva <dev.douggo@gmail.com>
2022-07-18 17:54:28 -0300

    Finalizada aula 03 - usando spring data

--- SpringBootApiRest/src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ SpringBootApiRest/src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -1,22 +1,26 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.alura.forum.controller.dto.TopicoDto;
-import br.com.alura.forum.model.Curso;
-import br.com.alura.forum.model.Topico;
+import br.com.alura.forum.model.dto.TopicoDto;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicoController {
     
+    @Autowired
+    private TopicoRepository repository;
+
     @RequestMapping("/topicos")
-    public List<TopicoDto> lista() {
-        Topico topico = new Topico("Dúvida", "Dúvida sobre Spring", new Curso("Spring Boot API Rest", "Programação"));
-        return TopicoDto.converter(Arrays.asList(topico, topico, topico));
+    public List<TopicoDto> lista(String nomeCurso) {
+        if(nomeCurso == null) {
+            return TopicoDto.converter(this.repository.findAll());
+        } 
+        return TopicoDto.converter(this.repository.findByCursoNome(nomeCurso));
     }
 
 }
