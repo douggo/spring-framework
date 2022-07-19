84f531b3763769c6e83f25658aac868f5d60802c
Douglas Felipe da Silva <dev.douggo@gmail.com>
2022-07-19 15:20:29 -0300

    Finalizada aula 01 - paginação e ordenação de recursos

--- SpringBootSCM/src/main/java/br/com/alura/forum/controller/TopicoController.java
+++ SpringBootSCM/src/main/java/br/com/alura/forum/controller/TopicoController.java
@@ -8,9 +8,9 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,12 +43,9 @@ public class TopicoController {
 
     @GetMapping
     public Page<TopicoDto> lista(
-        @RequestParam(required = false) String nomeCurso,
-        @RequestParam int pagina,
-        @RequestParam int quantidade,
-        @RequestParam String ordenacao
-    ) {
-        Pageable paginacao = PageRequest.of(pagina, quantidade, Direction.ASC, ordenacao);
+        @RequestParam(required = false) String nomeCurso, 
+        @PageableDefault(sort = "id", direction = Direction.ASC, page = 0, size = 10) Pageable paginacao
+    ) {    
         if(nomeCurso == null) {
             return TopicoDto.converter(this.repository.findAll(paginacao));
         } 
